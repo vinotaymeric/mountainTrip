2a3673bcf7a4f87c6fcac71d6022b93f89ba0e05
nicolas <nicolas.eymenier@gmail.com>
2019-02-18 19:03:04 +0100

    navbar javascript done

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,19 +1,21 @@
 const InitUpdateNavbar = () => {
   const navbar = document.querySelector('.navbar-mountain')
-
+  var scrollPos = 0;
   if (navbar == null) {
     return ;
   }
 
   document.addEventListener('scroll', (e) => {
-    console.log("je suis là")
-    const intViewportHeight = window.innerHeight;
 
-    if (window.scrollY < intViewportHeight/3) {
-      navbar.classList.remove("navbar-mountain")
-    } else {
-      navbar.classList.add("navbar-mountain")
+    const intViewportHeight = window.innerHeight;
+    if ((document.body.getBoundingClientRect()).top > scrollPos){
+      navbar.classList.remove("navbar-invisible") }
+     else if (window.scrollY < intViewportHeight/2.5) {
+      navbar.classList.remove("navbar-invisible")
+    } else  {
+      navbar.classList.add("navbar-invisible")
     }
+    scrollPos = (document.body.getBoundingClientRect()).top;
   })
 }
 
