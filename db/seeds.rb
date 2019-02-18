b3cd45a9c18996e7cc585911c0b683f9e13f7737
jordi Tabit <jordi.tabit@hotmail.fr>
2019-02-18 18:25:11 +0100

    Refacto seeding iti, seeding users and trip

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 require 'json'
 require 'open-uri'
 require 'nokogiri'
+require 'date'
 
 ## SEED ITINERARIES FROM CAMP_TO_CAMP
+puts "Seeding Itineraries..."
+
 def api_call(itinerary, id)
   url = "https://api.camptocamp.org/#{itinerary}/#{id.to_s}"
-  itinerary_hash = JSON.parse(open(url).read)
+  JSON.parse(open(url).read)
 end
 
 Itinerary.destroy_all
@@ -24,21 +27,22 @@ end
 
 itinerary_ids[0..30].each do |id|
   itinerary = Itinerary.new
-  itinerary.coord_x = api_call("routes", id)["geometry"]["geom"][17..-1].split(",")[0]
-  itinerary.coord_y = api_call("routes", id)["geometry"]["geom"][17..-1].split(",")[1][1..-2]
-  itinerary.diffculty = api_call("routes", id)["global_rating"]
-  itinerary.elevation_max = api_call("routes", id)["elevation_max"]
-  itinerary.height_diff_up = api_call("routes", id)["height_diff_up"]
-  itinerary.engagement_rating = api_call("routes", id)["engagement_rating"]
-  itinerary.equipment_rating = api_call("routes", id)["equipment_rating"]
-  itinerary.activities = api_call("routes", id)["activities"]
-  itinerary.orientations = api_call("routes", id)["orientations"]
+  itinerary_hash = api_call("routes", id)
+  itinerary.coord_x = itinerary_hash["geometry"]["geom"][17..-1].split(",")[0]
+  itinerary.coord_y = itinerary_hash["geometry"]["geom"][17..-1].split(",")[1][1..-2]
+  itinerary.diffculty = itinerary_hash["global_rating"]
+  itinerary.elevation_max = itinerary_hash["elevation_max"]
+  itinerary.height_diff_up = itinerary_hash["height_diff_up"]
+  itinerary.engagement_rating = itinerary_hash["engagement_rating"]
+  itinerary.equipment_rating = itinerary_hash["equipment_rating"]
+  itinerary.activities = itinerary_hash["activities"]
+  itinerary.orientations = itinerary_hash["orientations"]
 
-  if api_call("routes", id)["associations"]["images"][0] != nil
-    itinerary.picture_url = "https://media.camptocamp.org/c2corg-active/#{api_call("routes", id)["associations"]["images"][0]["filename"]}"
+  if itinerary_hash["associations"]["images"][0] != nil
+    itinerary.picture_url = "https://media.camptocamp.org/c2corg-active/#{itinerary_hash["associations"]["images"][0]["filename"]}"
   end
 
-  api_call("routes", id)["locales"].each do |locale|
+  itinerary_hash["locales"].each do |locale|
     if locale["lang"] == "fr" && locale["title"] != nil
       itinerary.title = "#{locale["title_prefix"]} #{locale["title"]}"
     elsif locale["lang"] == "fr" && locale["title"].nil?
@@ -48,15 +52,37 @@ itinerary_ids[0..30].each do |id|
 
   itinerary.number_of_outings = api_call("outings", id)["associations"]["recent_outings"]["total"]
   itinerary.save
-  sleep(2)
+  sleep(1)
+  print "."
 end
 
+puts "Itineraries seeding completed"
 
 ## SEED FAKE USERS
+puts "Seeding users..."
 
 User.destroy_all
 
+User.create!(user_name: "Nico", email: "nico@crux.io", password: "qwertyuiop")
+User.create!(user_name: "Aymeric", email: "aymeric@crux.io", password: "qwertyuiop")
+User.create!(user_name: "Ouramdane", email: "ouramadane@crux.io", password: "qwertyuiop")
+User.create!(user_name: "Jordi", email: "jordi@crux.io", password: "qwertyuiop")
+
+puts "User seeding completed"
 
 ## SEED FAKE TRIPS
+puts "Seeding trips..."
 
 Trip.destroy_all
+
+User.all.each do |cur_user|
+  cur_itinerary = Itinerary.all.sample
+  Trip.create!(start_date: DateTime.new(2019,3,10,8,0,0), end_date: DateTime.new(2019,3,10,19,0,0), user: cur_user, itinerary: cur_itinerary )
+end
+
+User.all.each do |cur_user|
+  cur_itinerary = Itinerary.all.sample
+  Trip.create!(start_date: DateTime.new(2019,3,15,8,0,0), end_date: DateTime.new(2019,3,15,19,0,0), user: cur_user, itinerary: cur_itinerary )
+end
+
+puts "Trip seeding completed"
