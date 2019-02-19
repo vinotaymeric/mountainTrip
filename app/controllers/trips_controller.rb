5e91a873b36a9d0344b3715e1f2c1600cd855677
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-02-19 13:27:01 +0100

    cleanup hotfix

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -11,26 +11,20 @@ class TripsController < ApplicationController
 
   def new
     @trip = Trip.new
-<<<<<<< HEAD
     @trip.itinerary = @itinerary
     @trip.user = current_user
-=======
     @itinerary = Itinerary.find(params[:itinerary_id])
->>>>>>> 38373390319f9c2a7f012a3cd0a123218afdf8a1
   end
 
   def create
     @trip = Trip.new(trip_params)
-<<<<<<< HEAD
     @trip.user = current_user
     @trip.save
-=======
     @trip.itinerary = Itinerary.find(params[:itinerary_id])
     # # Lignes dessous à modifier quand le login fonctionnera
     # @trip.user = current_user
     @trip.user = User.find(4)
     @trip.save!
->>>>>>> 38373390319f9c2a7f012a3cd0a123218afdf8a1
     redirect_to trips_path
   end
 
