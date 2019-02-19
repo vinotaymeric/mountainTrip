4e53a4a98c7c555a839bf2bc891b71805c633780
ouramdaneMakhoukh <oumakhoukh@gmail.com>
2019-02-19 01:06:33 +0100

    add navbar and flashes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :trips, dependent: :destroy
   has_many :user_trips
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable 
+         :recoverable, :rememberable, :validatable
 end
