54af6680b77926aaedd5fada4e2b143935d13424
nicolas <nicolas.eymenier@gmail.com>
2019-02-19 13:03:59 +0100

    javascript on every cards

--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -1,22 +1,23 @@
 const CardsHover = () => {
-  const card = document.querySelector('.card')
+  const cards = document.querySelectorAll('.card')
 
-  const cardUser = document.querySelector('.card-user')
-  const cardDescription = document.querySelector('.card-description')
-  const cardCategory = document.querySelector('.card-category')
+  cards.forEach((card) => {
+    const cardUser = card.querySelector('.card-user')
+    const cardDescription = card.querySelector('.card-description')
+    const cardCategory = card.querySelector('.card-category')
 
-  card.onmouseover = (e) => {
-    cardUser.classList.remove("card-element-invisible");
-    cardCategory.classList.remove("card-element-invisible");
-    cardDescription.classList.add("card-element-up");
-  }
+    card.onmouseover = (e) => {
+      cardUser.classList.remove("card-element-invisible");
+      cardCategory.classList.remove("card-element-invisible");
+      cardDescription.classList.add("card-element-up");
+    }
 
-  card.onmouseout= (e) => {
-    cardUser.classList.add("card-element-invisible");
-    cardCategory.classList.add("card-element-invisible");
-    cardDescription.classList.remove("card-element-up");
-  }
-
+    card.onmouseout= (e) => {
+      cardUser.classList.add("card-element-invisible");
+      cardCategory.classList.add("card-element-invisible");
+      cardDescription.classList.remove("card-element-up");
+    }
+  });
 
 }
 export default CardsHover;
