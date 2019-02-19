5135f07c48e24fb0c3ea4ace1a32966c51beab0f
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-02-19 09:51:33 +0100

    added title to trips

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :itineraries, only: [:index, :show] do
     resources :trips, only: [:new, :create]
   end
-  resources :trips, only: [:new, :create]
+  resources :trips, only: [:new, :create, :show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_143950) do
+ActiveRecord::Schema.define(version: 2019_02_19_085041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_143950) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["itinerary_id"], name: "index_trips_on_itinerary_id"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
@@ -60,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_02_18_143950) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
