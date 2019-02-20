2f00cadf6e71140e52c255035cd043d5e4e5b636
nicolas <nicolas.eymenier@gmail.com>
2019-02-20 15:53:29 +0100

    ok cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @trips = Trip.last(6)
+
   end
 end
--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -4,23 +4,26 @@ const CardsHover = () => {
   if (cards==null) {return;}
 
   cards.forEach((card) => {
-    const cardUser = card.querySelector('.card-user')
+    const cardUserAvatar = card.querySelector('.card-user-avatar')
+    const cardIcon = card.querySelector('.card-icon')
     const cardDescription = card.querySelector('.card-description')
     const cardCategory = card.querySelector('.card-category')
     const cardText = card.querySelector('.card-comment')
 
     card.onmouseover = (e) => {
-      cardUser.classList.remove("card-element-invisible");
+      cardUserAvatar.classList.remove("card-element-invisible");
       cardCategory.classList.remove("card-element-invisible");
       cardDescription.classList.add("card-element-up");
       cardText.classList.remove("card-element-invisible");
+      cardIcon.classList.remove("card-element-invisible");
     }
 
     card.onmouseout= (e) => {
-      cardUser.classList.add("card-element-invisible");
+      cardUserAvatar.classList.add("card-element-invisible");
       cardCategory.classList.add("card-element-invisible");
       cardDescription.classList.remove("card-element-up");
       cardText.classList.add("card-element-invisible");
+      cardIcon.classList.add("card-element-invisible");
     }
   });
 
