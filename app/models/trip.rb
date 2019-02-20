25d434c482c8585c7f01f4299dea6d542c0bec6a
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-02-20 10:32:52 +0100

    deleting a trip deletes its messages

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :itinerary
   has_many :user_trips
-  has_many :messages
+  has_many :messages, dependent: :destroy
   validates :start_date, presence: true
   validates :end_date, presence: true
   # validates :title, presence: true
