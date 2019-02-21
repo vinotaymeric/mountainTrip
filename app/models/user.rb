ee1ff05a021053d6fe660445e308db7fd00288f2
jordi Tabit <jordi.tabit@hotmail.fr>
2019-02-21 15:53:43 +0100

    Still not working fb , fetch master

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,22 @@ class User < ApplicationRecord
     user_params[:facebook_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+    user_params[:user_name] = auth.info[:first_name]
+    user_params[:email] = auth.info[:email]
+    # user_params[:picture] = user_params[:facebook_picture_url]
+    p
+    p
+    p user_params[:picture]
     user_params = user_params.to_h
-
     user = User.find_by(provider: auth.provider, uid: auth.uid)
     user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
     if user
       user.update(user_params)
     else
+      p "coucou"
+      p user_params
       user = User.new(user_params)
+      p user
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
       user.save
     end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2019_02_21_114746) do
     t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
     t.float "coord_long"
     t.float "coord_lat"
     t.text "content"
