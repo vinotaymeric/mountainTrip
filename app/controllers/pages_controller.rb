774f0c6568e2dd890bd8cb997d9dab50e0b099a5
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-02-21 12:47:36 +0100

    added container everywhere except homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
+  layout false
   def home
     @trips = Trip.last(6)
   end
 
   def profil
-    @user = current_user 
+    @user = current_user
   end
 end
